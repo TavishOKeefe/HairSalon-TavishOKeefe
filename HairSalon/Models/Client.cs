16caaecd78eeb3a55b4d3849b0be134897277ab3
Padma Patil <padma@email.com>
2018-12-07 10:44:34 -0800

    create object class, constructors, and properties, for client and stylist.

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    private string _name;
+    private int _id;
+    private int _sylistId;
+
+    public Client (string name, int sylistId , int id = 0)
+    {
+      _name = name;
+      _sylistId = sylistId;
+      _id = id;
+
+    }
+
+    public string GetClientName()
+    {
+      return _name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public int GetStylistId()
+    {
+      return _sylistId;
+    }
