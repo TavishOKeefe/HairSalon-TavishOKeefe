151eb2279615f3e1c5e346b8d4f6280b3b165414
Padma Patil <padma@email.com>
2018-12-07 16:04:25 -0800

    add Views files and initial controllers to display UI inputs.

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    [HttpGet("/stylist")]
+    public ActionResult Index()
+    {
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View(allStylists);
+    }
+
+    [HttpGet("/stylist/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/stylist")]
+    public ActionResult Create(string stylistName)
+    {
+      Stylist newStylist = new Stylist(stylistName);
+      newStylist.Save();
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View("Index", allStylists);
+    }
+  }
+}
